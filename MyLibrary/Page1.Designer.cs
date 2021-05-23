fa13169a9d6ca86d1bef8a13f1e92a6ea7c1cfcc
Tamer Köşe <tamerkose67@gmail.com>
2021-05-23 18:50:10 +0300

    Book connection was done.We can insert and select from sql for book.

--- MyLibrary/Page1.Designer.cs
+++ MyLibrary/Page1.Designer.cs
@@ -33,179 +33,214 @@ namespace MyLibrary
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtBook = new System.Windows.Forms.TextBox();
+            this.txtPageCount = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.txtISBN = new System.Windows.Forms.TextBox();
+            this.btnAddBook = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboxLan = new System.Windows.Forms.ComboBox();
+            this.comboxCategory = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtAutName = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtAutSurname = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(11, 20);
+            this.label1.Location = new System.Drawing.Point(10, 15);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(87, 20);
+            this.label1.Size = new System.Drawing.Size(69, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "Book Name";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(11, 69);
+            this.label2.Location = new System.Drawing.Point(10, 52);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(84, 20);
+            this.label2.Size = new System.Drawing.Size(69, 15);
             this.label2.TabIndex = 1;
             this.label2.Text = "Page Count";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(11, 117);
+            this.label3.Location = new System.Drawing.Point(10, 88);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(69, 20);
+            this.label3.Size = new System.Drawing.Size(55, 15);
             this.label3.TabIndex = 2;
             this.label3.Text = "Category";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(11, 164);
+            this.label4.Location = new System.Drawing.Point(10, 123);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(54, 20);
+            this.label4.Size = new System.Drawing.Size(79, 15);
             this.label4.TabIndex = 3;
-            this.label4.Text = "Author";
+            this.label4.Text = "Author Name";
             // 
-            // textBox1
+            // txtBook
             // 
-            this.textBox1.Location = new System.Drawing.Point(99, 17);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(125, 27);
-            this.textBox1.TabIndex = 4;
+            this.txtBook.Location = new System.Drawing.Point(110, 13);
+            this.txtBook.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtBook.Name = "txtBook";
+            this.txtBook.Size = new System.Drawing.Size(110, 23);
+            this.txtBook.TabIndex = 4;
             // 
-            // textBox2
+            // txtPageCount
             // 
-            this.textBox2.Location = new System.Drawing.Point(99, 66);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(125, 27);
-            this.textBox2.TabIndex = 5;
+            this.txtPageCount.Location = new System.Drawing.Point(110, 52);
+            this.txtPageCount.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtPageCount.Name = "txtPageCount";
+            this.txtPageCount.Size = new System.Drawing.Size(110, 23);
+            this.txtPageCount.TabIndex = 5;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(13, 213);
+            this.label5.Location = new System.Drawing.Point(11, 206);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(67, 20);
+            this.label5.Size = new System.Drawing.Size(53, 15);
             this.label5.TabIndex = 8;
             this.label5.Text = "ISBN NO";
             // 
-            // textBox5
+            // txtISBN
             // 
-            this.textBox5.Location = new System.Drawing.Point(99, 210);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(125, 27);
-            this.textBox5.TabIndex = 9;
+            this.txtISBN.Location = new System.Drawing.Point(110, 198);
+            this.txtISBN.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtISBN.Name = "txtISBN";
+            this.txtISBN.Size = new System.Drawing.Size(110, 23);
+            this.txtISBN.TabIndex = 9;
             // 
-            // button1
+            // btnAddBook
             // 
-            this.button1.Location = new System.Drawing.Point(90, 324);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 29);
-            this.button1.TabIndex = 10;
-            this.button1.Text = "Add ";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnAddBook.Location = new System.Drawing.Point(11, 284);
+            this.btnAddBook.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnAddBook.Name = "btnAddBook";
+            this.btnAddBook.Size = new System.Drawing.Size(82, 22);
+            this.btnAddBook.TabIndex = 10;
+            this.btnAddBook.Text = "Add ";
+            this.btnAddBook.UseVisualStyleBackColor = true;
+            this.btnAddBook.Click += new System.EventHandler(this.btnAddBook_Click);
             // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.Red;
             this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button2.Location = new System.Drawing.Point(380, 277);
+            this.button2.Location = new System.Drawing.Point(335, 243);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.Size = new System.Drawing.Size(82, 22);
             this.button2.TabIndex = 11;
             this.button2.Text = "Delete Book";
             this.button2.UseVisualStyleBackColor = false;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(90, 378);
+            this.button3.Location = new System.Drawing.Point(129, 284);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(94, 29);
+            this.button3.Size = new System.Drawing.Size(82, 22);
             this.button3.TabIndex = 12;
             this.button3.Text = "Search";
             this.button3.UseVisualStyleBackColor = true;
             // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 20;
-            this.listBox1.Location = new System.Drawing.Point(272, 20);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(326, 224);
-            this.listBox1.TabIndex = 13;
-            // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(11, 262);
+            this.label6.Location = new System.Drawing.Point(10, 243);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(74, 20);
+            this.label6.Size = new System.Drawing.Size(59, 15);
             this.label6.TabIndex = 14;
             this.label6.Text = "Language";
             // 
-            // comboBox1
+            // comboxLan
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(99, 259);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(151, 28);
-            this.comboBox1.TabIndex = 15;
+            this.comboxLan.FormattingEnabled = true;
+            this.comboxLan.Location = new System.Drawing.Point(87, 235);
+            this.comboxLan.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboxLan.Name = "comboxLan";
+            this.comboxLan.Size = new System.Drawing.Size(133, 23);
+            this.comboxLan.TabIndex = 15;
+            this.comboxLan.Click += new System.EventHandler(this.comboxLan_Click);
             // 
-            // comboBox2
+            // comboxCategory
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(99, 164);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(151, 28);
-            this.comboBox2.TabIndex = 16;
+            this.comboxCategory.FormattingEnabled = true;
+            this.comboxCategory.Location = new System.Drawing.Point(87, 88);
+            this.comboxCategory.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboxCategory.Name = "comboxCategory";
+            this.comboxCategory.Size = new System.Drawing.Size(133, 23);
+            this.comboxCategory.TabIndex = 17;
+            this.comboxCategory.SelectedIndexChanged += new System.EventHandler(this.comboxCategory_SelectedIndexChanged);
+            this.comboxCategory.Click += new System.EventHandler(this.comboxCategory_Click);
             // 
-            // comboBox3
+            // dataGridView1
             // 
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(99, 117);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(151, 28);
-            this.comboBox3.TabIndex = 17;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(226, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(315, 225);
+            this.dataGridView1.TabIndex = 18;
+            // 
+            // txtAutName
+            // 
+            this.txtAutName.Location = new System.Drawing.Point(110, 123);
+            this.txtAutName.Name = "txtAutName";
+            this.txtAutName.Size = new System.Drawing.Size(110, 23);
+            this.txtAutName.TabIndex = 19;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(10, 160);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(94, 15);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Author Surname";
+            // 
+            // txtAutSurname
+            // 
+            this.txtAutSurname.Location = new System.Drawing.Point(110, 160);
+            this.txtAutSurname.Name = "txtAutSurname";
+            this.txtAutSurname.Size = new System.Drawing.Size(110, 23);
+            this.txtAutSurname.TabIndex = 21;
             // 
             // Page1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.comboBox3);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtAutSurname);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtAutName);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboxCategory);
+            this.Controls.Add(this.comboxLan);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.btnAddBook);
+            this.Controls.Add(this.txtISBN);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtPageCount);
+            this.Controls.Add(this.txtBook);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Page1";
-            this.Size = new System.Drawing.Size(628, 450);
+            this.Size = new System.Drawing.Size(550, 338);
+            this.Load += new System.EventHandler(this.Page1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -217,17 +252,19 @@ namespace MyLibrary
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtBook;
+        private System.Windows.Forms.TextBox txtPageCount;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtISBN;
+        private System.Windows.Forms.Button btnAddBook;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboxLan;
+        private System.Windows.Forms.ComboBox comboxCategory;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtAutName;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtAutSurname;
     }
 }
--- MyLibrary/Page1.cs
+++ MyLibrary/Page1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
@@ -10,9 +11,122 @@ namespace MyLibrary
 {
     public partial class Page1 : UserControl
     {
+        SqlConnection con = new SqlConnection(@"Data Source=PC;Initial Catalog=MyVisualLibrary;Integrated Security=True");
+        SqlCommand com;
+        SqlDataReader dr, dr2;
+
         public Page1()
         {
             InitializeComponent();
+        }
+
+        private void Page1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddBook_Click(object sender, EventArgs e)
+        {
+            string BookName = txtBook.Text;
+            int PageCount = Convert.ToInt32(txtPageCount.Text);
+            string category = (string)comboxCategory.SelectedItem;
+            string AutName = txtAutName.Text;
+            string AutSurname = txtAutSurname.Text;
+            int numISBN = Convert.ToInt32(txtISBN.Text);
+            string language = (string)comboxLan.SelectedItem;
+
+            try
+            {
+                con.Open();
+                bool hasRow = false;
+                using (SqlCommand check_book_name = new SqlCommand("select BOOK_NAME from dbo.BOOK where BOOK_NAME ='" + BookName + "'", con))
+                using (SqlDataReader reader = check_book_name.ExecuteReader())
+                {
+                    hasRow = reader.HasRows;
+                }
+                if (hasRow == true)
+                {
+                    MessageBox.Show("Test");
+                }
+                else
+                {
+                    com = new SqlCommand();
+                    com.Connection = con;
+                    com.CommandText = "insert into dbo.BOOK(BOOK_NAME,BOOK_PAGE_COUNT,BOOK_ISBN_NO,LANGUAGE) values(@param1,@param2,@param3,@param4)";
+                    com.Parameters.Add("@param1", SqlDbType.VarChar, 50).Value = BookName;
+                    com.Parameters.Add("@param2", SqlDbType.Int).Value = PageCount;
+                    com.Parameters.Add("@param3", SqlDbType.Int).Value = numISBN;
+                    com.Parameters.Add("@param4", SqlDbType.VarChar, 10).Value = language;
+                    com.CommandType = CommandType.Text;
+                    com.ExecuteNonQuery();
+                    MessageBox.Show("book başarıyla eklendi");
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+
+            con.Close();
+
+        }
+
+        private void comboxCategory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            /* com = new SqlCommand();
+             con.Open();
+             com.Connection = con;
+             com.CommandText = "select CATEGORY_NAME from dbo.CATEGORY";
+             dr = com.ExecuteReader();
+
+             while (dr.Read())
+             {
+                 comboxCategory.Items.Add(dr["CATEGORY_NAME"]);
+             }
+
+             con.Close();*/
+        }
+
+        private void comboxCategory_Click(object sender, EventArgs e)
+        {
+            com = new SqlCommand();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select CATEGORY_NAME from dbo.CATEGORY";
+            dr = com.ExecuteReader();
+
+            while (dr.Read())
+            {
+                comboxCategory.Items.Add(dr["CATEGORY_NAME"]);
+            }
+
+            dr.Close();
+            con.Close();
+        }
+
+        private void comboxLan_Click(object sender, EventArgs e)
+        {
+            comboxLan.Items.Add("tr");
+            comboxLan.Items.Add("eng");
+            comboxLan.Items.Add("fr");
+        }
+
+        private void comboxAuthor_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            /*string SqlDataPull = ("select AUTHOR_NAME, AUTHOR_SURNAME from dbo.AUTHOR");
+            SqlCommand cmd = new SqlCommand(SqlDataPull);
+            cmd.CommandType = CommandType.Text;
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                SqlDataPull = dr[0].ToString();
+                comboxLan.Items.Add(SqlDataPull);
+            }
+
+            dr.Close();   */
         }
     }
 }
